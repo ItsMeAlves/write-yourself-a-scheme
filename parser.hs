a0f3b9cf4b6473e1b81dd874d68e309068fb8bb0
Gabriel Alves <itsmealves@gmail.com>
2017-06-16 14:16:27 -0300

    Simple symbol parser

--- parser.hs
+++ parser.hs
@@ -3,6 +3,15 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+symbol :: Parser Char
+symbol = oneOf "!$ %&|*+ -/: <=? > @^_ ~#"
+
+readExpression :: String -> String
+readExpression input = parseResult $ parse symbol "lisp" input
+  where parseResult (Left err) = "No match: " ++ show err
+        parseResult (Right err) = "Found value"
+
 main :: IO ()
 main = do
-  putStrLn "Hello!"
+  args <- getArgs
+  putStrLn $ readExpression (args !! 0)
