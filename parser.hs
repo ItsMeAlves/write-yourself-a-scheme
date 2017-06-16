dfa2eb67a4653405880448e6661d0cf8c01016ed
Gabriel Alves <itsmealves@gmail.com>
2017-06-16 18:07:58 -0300

    Adding Float and Digital parsers

--- parser.hs
+++ parser.hs
@@ -7,13 +7,14 @@ data LispValue =
   Atom String
   | String String
   | Boolean Bool
+  | Float Float
   | Number Integer
   | List [LispValue]
   | DottedList [LispValue] LispValue
   deriving (Show)
 
 symbol :: Parser Char
-symbol = oneOf "!$ %&|*+ -/: <=? > @^_ ~#"
+symbol = oneOf "!$ %&|*+ -/: <=? > @^_ ~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -26,23 +27,49 @@ parseString = do
   return $ String x
 
 parseNumber :: Parser LispValue
-parseNumber = (many1 digit) >>= return . Number . read
+parseNumber = parseInteger
+  <|> parseDigital
+
+parseInteger :: Parser LispValue
+parseInteger = (many1 digit) >>= return . Number . read
+
+parseDigital :: Parser LispValue
+parseDigital = do
+  try $ string "#d"
+  num <- many1 digit
+  (return . Number . read) num
 
 parseAtom :: Parser LispValue
 parseAtom = do
   first <- letter <|> symbol
   rest <- many (letter <|> symbol <|> digit)
-  return $ booleanIdentification (first : rest)
-  where booleanIdentification "#t" = Boolean True
-        booleanIdentification "#f" = Boolean False
-        booleanIdentification s = Atom s
+  return $ Atom (first : rest)
+
+parseBoolean :: Parser LispValue
+parseBoolean = do
+  string "#"
+  value <- oneOf "tf"
+  (return . Boolean) $ booleanIdentification value
+  where booleanIdentification 't' = True
+        booleanIdentification 'f' = False
+
+parseFloat :: Parser LispValue
+parseFloat = do
+  left <- many1 digit
+  try $ char '.'
+  right <- many1 digit
+  (return . Float . read) (left ++ "." ++ right)
 
 readExpression :: String -> String
 readExpression input = parseResult $ parse parser "lisp" input
   where parseResult (Left err) = "No match: " ++ show err
         parseResult (Right val) = "Found value: " ++ show val
-        parser = parseAtom <|> parseString <|> parseNumber
-
+        parser = parseAtom
+          <|> parseString
+          <|> try parseFloat
+          <|> parseNumber
+          <|> parseBoolean
+        
 main :: IO ()
 main = do
   args <- getArgs
